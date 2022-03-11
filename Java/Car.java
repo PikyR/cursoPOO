ac8929a5853d7d65576174e7eaea6d4c50426417
pikyr <pikytdv@gmail.com>
2022-03-10 21:34:11 -0300

    Fin de clase 32 - Encapsulando atributos en Java

--- Java/Car.java
+++ Java/Car.java
@@ -2,15 +2,33 @@ public class Car {
   Integer id;
   String license;
   Account driver;
-  Integer passengers;
+  private Integer passengers;
 
   // Metodo constructor
   public Car(String license, Account driver){
     this.license = license;
-    this.driver = driver;
+    this.driver = driver;    
   }
 
   void printCarData() {
-    System.out.println("Driver: " + driver.name + " - " + "License: " + license + " - " + "Passengers: " + passengers);
+    if (passengers != null) {
+      System.out.println(
+        "Driver: " + driver.name + " - " + 
+        "License: " + license + " - " + 
+        "Passengers: " + passengers
+      );
+    }
+  }
+
+  public Integer getPassengers(){
+    return passengers;
+  }
+
+  public void setPassengers(Integer passengers){
+    if (passengers == 4) {
+      this.passengers = passengers;
+    } else {
+      System.out.println("Necesitas asignar 4 pasajeros.");
+    }
   }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,16 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    Car firstCar = new Car("ERS555", new Account("Roberto Rosas", "ROB456"));
-    firstCar.passengers = 4;
+    Car firstCar = new Car("ERS555", new Account("Roberto Rosas", "ROB456"));    
+    firstCar.setPassengers(2);
     firstCar.printCarData();
 
     Car secondCar = new Car("POU999", new Account("Carlos Comue", "CAR989"));
-    secondCar.passengers = 2;
+    secondCar.setPassengers(1);
     secondCar.printCarData();
 
-    // Clase 31
-    CarBasic firsBasic = new CarBasic("RRR555", new Account("Jonas Jon", "JJ88"), "SuperMarca", "SuperModelo");
-    firsBasic.passengers = 99;
+    // Clase 31 - 32
+    CarBasic firsBasic = new CarBasic("RRR555", new Account("Jonas Jon", "JJ88"), "SuperMarca", "SuperModelo");    
+    firsBasic.setPassengers(4);
     firsBasic.printCarData();
   }
 }
